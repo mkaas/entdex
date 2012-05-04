91466ee31d4dd81140b4a99c5203f402e816b776
mkaas <mikkel@uniqma.dk>
2012-05-04 11:58:45 +0200

    - Signed-off-by:mkaas <mikkel@uniqma.dk>

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -1,7 +1,29 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+	function htmlHeader(){
+		include_once('includes/db.inc.php');
+		$appSQL=mysql_query("SELECT * FROM ed_appsetup WHERE app_active='true'") or die(mysql_error());
+		while($arAPP=mysql_fetch_array($appSQL)){
+			$title=$arAPP['app_title'];
+			$version=$arAPP['app_version'];
+			$revision=$arAPP['app_revision'];
+			$theme=$arAPP['app_theme'];
+		}
+		print '
+			<html>
+				<head>
+					<title>'.$title.' '.$version.''.$revision.'</title>
+					<link rel="stylesheet" type="text/css" href="css/'.$theme.'.css" />
+				</head>
+				<body>
+		';
+	}
+
+	function htmlFooter(){
+		print '
+				</body>
+			</html>
+		';
+	}
+	
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+	include_once('includes/functions.inc.php');
+	htmlHeader();
 	
+	
+	
+	htmlFooter();
 
 ?>
