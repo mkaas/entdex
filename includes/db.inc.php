492d4b24f100397625732de265de5b5679240c79
mkaas <mikkel@uniqma.dk>
2012-05-05 14:01:53 +0200

    - Signed-off-by:mkaas <mikkel@uniqma.dk>

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -3,8 +3,8 @@
 	$dbhost = 'localhost';
 	$dbname = 'entdex';
 	
-	$dbuser = 'root';
-	$dbpass = '';
+	$dbuser = 'webdev';
+	$dbpass = 'webdev';
 
 	mysql_connect($dbhost,$dbuser,$dbpass) or die(mysql_error());
 	mysql_select_db($dbname) or die(mysql_error());
