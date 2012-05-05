2bf1bd376a928ed6326daeb0b967696247949b7b
mkaas <mikkel@uniqma.dk>
2012-05-05 15:09:03 +0200

    - Signed-off-by:mkaas <mikkel@uniqma.dk>

--- includes/commands.inc.php
+++ includes/commands.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-	$cpuspeed = "sudo dmidecode -t4 | grep 'Max Speed' | head -1 | cut -d ':' -f 2"
-	
+	$cpuspeed = "sudo dmidecode -t4 | grep 'Max Speed' | head -1 | cut -d ':' -f 2";
+	$distro_release = "lsb_release -a | grep Description | cut -d ':' -f 2";
 
 ?>
